020070ef16e1cc5933fbb557f0596329bed44ab3
mohana-pamidi <mohanashree.pamidimukkala@gmail.com>
2026-02-20 21:22:44 -0500

    added .gitignore, gemini connection now works

--- backend/gemini_client.py
+++ backend/gemini_client.py
@@ -2,26 +2,41 @@
 import google.generativeai as genai
 import os 
 from dotenv import load_dotenv
+import time
+from google.api_core import exceptions
 
-class gemini_client:
+class GeminiClient:
 
     def __init__(self): 
         load_dotenv()
 
         API_KEY = os.getenv("API_KEY")
         genai.configure(api_key=API_KEY)
-        
-        self.model = genai.GenerativeModel("gemini-3.1-pro")
+
+        for model in genai.list_models():
+            print(model.name)
+      
+        self.model = genai.GenerativeModel("gemini-2.5-flash") # change to gemini-2.5-pro later for actual impelemntation
         self.prompt = None 
         self.response = None
 
-    def load_prompt(self, prompt):
-        with open("yourfile.txt", "r", encoding="utf-8") as f:
+    def load_prompt(self):
+        with open("prompt.txt", "r", encoding="utf-8") as f:
             self.prompt = f.read()
         
-    def makeAPICall(self, prompt):
+    def makeAPICall(self):
         #we must send append prom
-        self.response = self.model.generate_content(prompt)
+        self.response = self.model.generate_content(self.prompt)
+        return self.response.text
+        # try:
+        #     self.response = self.model.generate_content(self.prompt)
+        #     return self.response.text
+        # except exceptions.ResourceExhausted:
+        #     print("Quota hit! Sleeping for 60 seconds...")
+        #     time.sleep(60)
+        #     return self.makeAPICall() # Try again
+
+
 
 
 
